0de235721ede55c6da9c7e47709f98f61ec9c872
YixingShen <kkman3651878@hotmail.com>
2020-12-29 18:12:13 +0800

    update

--- Circuits_Sequential_Logic/Finite_State_Machines/examsece241_2013_q4.v
+++ Circuits_Sequential_Logic/Finite_State_Machines/examsece241_2013_q4.v
@@ -10,5 +10,53 @@ module top_module (
     output fr1,
     output dfr
 ); 
+    parameter LevelA = 0, LevelB = 1, LevelC = 2, LevelD = 3;
+    
+    reg [2:0] state, next_state;
+    reg dfr_t;
+    wire [2:0] fr;
 
+    assign dfr = dfr_t;
+    assign {fr3,fr2,fr1} = fr;
+
+    always @(*) begin
+        if (s[3] == 1 && s[2] == 1 && s[1] == 1) begin
+            next_state = LevelD;
+        end
+        else if (s[2] == 1 && s[1] == 1) begin
+            next_state = LevelC;
+        end
+        else if (s[1] == 1) begin
+            next_state = LevelB;
+        end
+        else begin
+            next_state = LevelA;
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            state <= 0;
+            dfr_t <= 1;
+        end
+        else begin
+            state <= next_state;
+
+            if (next_state < state)
+                dfr_t <= 1;
+            else if (next_state > state)
+                dfr_t <= 0;
+        end
+    end
+    
+    always @(*) begin
+        case (state)
+            LevelA: fr = 3'b111;
+            LevelB: fr = 3'b011;
+            LevelC: fr = 3'b001;
+            LevelD: fr = 3'b000;
+            default: fr = 3'b111;
+        endcase
+    end
+       
 endmodule
---
+++ Circuits_Sequential_Logic/Finite_State_Machines/lemmings1.v
@@ -0,0 +1,54 @@
+//The game Lemmings involves critters with fairly simple brains.
+//So simple that we are going to model it using a finite state machine.
+
+//In the Lemmings' 2D world, Lemmings can be in one of two states: 
+//walking left or walking right. It will switch directions if it hits an obstacle. 
+//In particular, if a Lemming is bumped on the left, it will walk right.
+//If it's bumped on the right, it will walk left. If it's bumped on both sides at the same time, it will still switch directions.
+
+//Implement a Moore state machine with two states, two inputs, and one output that models this behaviour.
+
+//See also: Lemmings2, Lemmings3, and Lemmings4.
+
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    output walk_left,
+    output walk_right); //  
+
+     parameter LEFT=0, RIGHT=1;
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        if (state == LEFT) begin
+            if (bump_left)
+                next_state = RIGHT;
+            else
+                next_state = LEFT;
+        end
+        else begin
+            if (bump_right)
+                next_state = LEFT;
+            else
+                next_state = RIGHT;
+        end
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+        if (areset) begin
+            state <= LEFT;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+    // Output logic
+    assign walk_left = (state == LEFT) ? 1 : 0;
+    assign walk_right = (state == RIGHT) ? 1 : 0;
+
+endmodule
